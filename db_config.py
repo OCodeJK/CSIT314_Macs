7c13591581fa6d3ba19d196b9eeba167770a291f
seeyu44 <seeyu44@gmail.com>
2025-05-08 16:39:47 +0800

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,8 +3,8 @@ import psycopg2
 def db_connection():
     return psycopg2.connect(
         dbname='c2c_db',
-        user='help',
-        password='1234',
+        user='admin',
+        password='123',
         host='localhost',
         port='5432'
     )
