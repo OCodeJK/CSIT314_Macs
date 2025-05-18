f27594bd7272f689ac24a54564061d19bde426cc
marcus <marcuschainwz@gmail.com>
2025-05-18 17:24:26 +0800

    added missing class name in cleaner controller

--- control/ShortlistCountForCleanerController.py
+++ control/ShortlistCountForCleanerController.py
@@ -1,7 +1,8 @@
 from entity.Shortlist import Shortlist
 
-def getNumberOfShortlistedTime(self, cleanerId, serviceId=None): 
-    try:
-        return Shortlist.numberOfShortlistedTime(cleanerId, serviceId)
-    except Exception:
-        return 0
+class ShortlistCountForCleanerController:
+    def getNumberOfShortlistedTime(self, cleanerId, serviceId=None): 
+        try:
+            return Shortlist.numberOfShortlistedTime(cleanerId, serviceId)
+        except Exception:
+            return 0
