de8dae3f8aa69d4df85d7874b1c08955959e6f00
“Gloria <gloriadinh@gmail.com>
2025-05-19 13:25:03 +0800

    fix shortlistcontrol

--- control/ShortlistCountForCleanerController.py
+++ control/ShortlistCountForCleanerController.py
@@ -1,7 +1,8 @@
 from entity.Shortlist import Shortlist
+class ShortlistCountForCleanerController:
 
-def getNumberOfShortlistedTime(self, cleanerId, serviceId=None): 
-    try:
-        return Shortlist.numberOfShortlistedTime(cleanerId, serviceId)
-    except Exception:
-        return 0
+    def getNumberOfShortlistedTime(self, cleanerId, serviceId=None): 
+        try:
+            return Shortlist.numberOfShortlistedTime(cleanerId, serviceId)
+        except Exception:
+            return 0
